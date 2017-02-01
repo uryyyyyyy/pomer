70dcdbc2bcd514b74127d8ed013325ecefe7bb99
uryyyyyyy <koki305@gmail.com>
2017-02-02 00:27:08 +0900

    kotlin

---
+++ android/android/app/src/main/java/com/github/uryyyyyyy/pomer/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.github.uryyyyyyy.pomer
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String? {
+        return "pomer"
+    }
+}

cb145b1278fd3e749942ea8eecc17b9b3c6e421d
uryyyyyyy <koki305@gmail.com>
2017-02-02 10:30:17 +0900

    firebase analytics

--- android/android/app/src/main/java/com/github/uryyyyyyy/pomer/toast/MyToastModule.kt
+++ android/android/app/src/main/java/com/github/uryyyyyyy/pomer/toast/MyToastModule.kt
@@ -1,11 +1,14 @@
 package com.github.uryyyyyyy.pomer.toast
 
+import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.UiThreadUtil
 import com.facebook.react.common.MapBuilder
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MyToastModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -22,6 +25,15 @@ class MyToastModule(reactContext: ReactApplicationContext) : ReactContextBaseJav
 
     @ReactMethod
     fun show(message: String, duration: Int) {
+        Log.i("myActivitysssss", "toast")
+        val mFirebaseAnalytics = FirebaseAnalytics.getInstance(reactApplicationContext)
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name")
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        Log.i("myActivitysssss", mFirebaseAnalytics.toString())
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+
         UiThreadUtil.runOnUiThread { Toast.makeText(reactApplicationContext, message, duration).show() }
     }
 }
